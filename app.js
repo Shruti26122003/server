a56f7c20e954ae4eafd0d255617ea5e8d28e519e
Shruti26122003 <shrutim8141@gmail.com>
2024-11-25 02:10:45 +0530

    backend of the application changes and added

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ connectDB();
 app.use("/api", authRoutes);
 app.use('/api', dashboardRoutes);
 app.use('/api', appointmentRoutes);  // Correct the route for appointments
-app.use('/api', periodRoute);  // Corrected route for period data
-
+app.use('/api', periodRoute);
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
